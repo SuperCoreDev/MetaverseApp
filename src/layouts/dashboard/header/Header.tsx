d35bf9aa3977968ee1573703e22d71daff2ffc8c
SuperCoreDev <coredevsantos@gmail.com>
2023-10-31 13:14:07 -0400

    metaverse_header_list

--- src/layouts/dashboard/header/Header.tsx
+++ src/layouts/dashboard/header/Header.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import NextLink from 'next/link';
 // @mui
 import { useTheme } from '@mui/material/styles';
-import { Stack, AppBar, Toolbar, Link , Box , IconButton } from '@mui/material';
+import { Stack, AppBar, Toolbar, Link, Box, IconButton } from '@mui/material';
 // utils
 import { bgBlur } from '../../../utils/cssStyles';
 // hooks
@@ -21,6 +21,9 @@ import AccountPopover from './AccountPopover';
 import LanguagePopover from './LanguagePopover';
 import ContactsPopover from './ContactsPopover';
 import NotificationsPopover from './NotificationsPopover';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
 // ----------------------------------------------------------------------
 
 type Props = {
@@ -30,6 +33,12 @@ type Props = {
 export default function Header({ onOpenNav }: Props) {
   const theme = useTheme();
 
+  const currentUrlPath = usePathname();
+  const [isMetaverseOpened, setMetaverseOpened] = useState(false);
+  useEffect(() => {
+    if (currentUrlPath.endsWith('/two/') || currentUrlPath.indexOf('metaverse')) setMetaverseOpened(true);
+    else setMetaverseOpened(false);
+  }, [currentUrlPath])
   const { themeLayout } = useSettingsContext();
 
   const isNavHorizontal = themeLayout === 'horizontal';
@@ -41,23 +50,30 @@ export default function Header({ onOpenNav }: Props) {
   const isOffset = useOffSetTop(HEADER.H_DASHBOARD_DESKTOP) && !isNavHorizontal;
 
   const renderContent = (
-    <>
+    <Stack sx={{ width: '100%' }} flexDirection='row' justifyContent='space-between'>
       {/* {isDesktop && isNavHorizontal && <Logo sx={{ mr: 2.5 }} />} */}
+      <Stack direction='row' alignItems='center'>
+        {!isDesktop && (
+          <IconButton onClick={onOpenNav} sx={{ mr: 1, color: 'text.primary' }}>
+            <Iconify icon="ei:navicon" width={30} color='lightgrey' />
+          </IconButton>
+        )}
 
-      {!isDesktop && (
-        <IconButton onClick={onOpenNav} sx={{ mr: 1, color: 'text.primary' }}>
-          <Iconify icon="ei:navicon" width={30} color='lightgrey' />
-        </IconButton>
-      )}
-
-      {/* <Searchbar /> */}
-      <Link href='/dashboard/one' rel="noopener" underline="none">
-        <Image alt="logo" src="/assets/logo.png" width={44.4} height={40}/>
-      </Link>
-      <Box sx={{padding:'10px'}}/>
-      <Image alt="logoCaption" src="/assets/logoCaption.png" width={108} height={22}/>
+        {/* <Searchbar /> */}
+        {isDesktop && (
+          <Link href='/dashboard/one' rel="noopener" underline="none">
+            <Image alt="logo" src="/assets/logo.png" width={44.4} height={40} />
+          </Link>)}
+        <Box sx={{ padding: '10px' }} />
+        <Image alt="logoCaption" src="/assets/logoCaption.png" width={108} height={22} />
+      </Stack>
+      {isMetaverseOpened && (<Stack gap="24px" direction='row' alignItems='center' fontFamily='Aeonik' fontSize={14} fontWeight={400}>
+        <Link href='/metaverse/overview'>Overview</Link>
+        <Link href='/metaverse/marketplace'>Marketplace</Link>
+        <Link href='/metaverse/buildmetaverse'>Build Metaverse</Link>
+      </Stack>)}
       <Stack
-        flexGrow={1}
+        // flexGrow={1}
         direction="row"
         alignItems="center"
         justifyContent="flex-end"
@@ -71,7 +87,7 @@ export default function Header({ onOpenNav }: Props) {
 
         <AccountPopover />
       </Stack>
-    </>
+    </Stack>
   );
 
   return (
@@ -88,7 +104,7 @@ export default function Header({ onOpenNav }: Props) {
         }),
         ...(isDesktop && {
           // width: `calc(100% - ${NAV.W_DASHBOARD + 1}px)`,
-          width:'100%',
+          width: '100%',
           // height: HEADER.H_DASHBOARD_DESKTOP,
           ...(isOffset && {
             height: HEADER.H_DASHBOARD_DESKTOP_OFFSET,
@@ -101,7 +117,7 @@ export default function Header({ onOpenNav }: Props) {
           }),
           ...(isNavMini && {
             // width: `calc(100% - ${NAV.W_DASHBOARD_MINI + 1}px)`,
-            width:'100%'
+            width: '100%'
           }),
         }),
       }}
@@ -110,7 +126,7 @@ export default function Header({ onOpenNav }: Props) {
         sx={{
           height: 1,
           px: { lg: 5 },
-          background : '#202324',
+          background: '#202324',
           boxShadow: '0px 6px 12px 0px #00000033'
         }}
       >
---
+++ src/pages/metaverse/buildmetaverse.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+// next
+import Head from 'next/head';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useRef,ReactNode,useState } from 'react';
+import { Icon } from '@iconify/react';
+import { Container, Typography, Stack } from '@mui/material';
+// layouts
+import DashboardLayout from '../../layouts/dashboard';
+// components
+
+
+
+
+
+BuildMetaverse.getLayout = (page: React.ReactElement) => <DashboardLayout>{page}</DashboardLayout>;
+
+// ----------------------------------------------------------------------
+export default function BuildMetaverse() {
+
+  return (
+    <>
+      <Head>
+        <title> Build Metaverse </title>
+      </Head>
+      <Stack sx={{width:'100%' , height:'100vh' , boxShadow: '0px 6px 12px 0px #00000033',
+        background: '#202324'}}>
+            Build Metaverse
+      </Stack>
+    </>)
+}
