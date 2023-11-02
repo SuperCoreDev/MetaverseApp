ea34d49e8927a1b85b415cd36d741e15463159aa
devking877 <devking877@gmail.com>
2023-11-02 09:34:56 -0400

    buildmetaverse

--- src/pages/metaverse/buildmetaverse.tsx
+++ src/pages/metaverse/buildmetaverse.tsx
@@ -5,16 +5,99 @@ import Head from 'next/head';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { useRef,ReactNode,useState } from 'react';
 import { Icon } from '@iconify/react';
-import { Container, Typography, Stack } from '@mui/material';
+import { Container, Typography, Stack, TextField, InputAdornment } from '@mui/material';
 // layouts
 import DashboardLayout from '../../layouts/dashboard';
+import MetaverseCard from './MetaverseCard';
+import { styled } from '@mui/material/styles';
 // components
 
-
-
-
+const MetaverseItem = [
+  {
+      'image': '/assets/nfts/MetaverseCards/Real Estate.png',
+      'name': 'Real Estate',
+      'content': `With Metaverse Estates, you have the power to design, customize, and inhabit your dream home in the metaverse. Want a castle in the clouds or a beachfront villa surrounded by digital waves....`,
+  },
+  {
+    'image': '/assets/nfts/MetaverseCards/ECommerce.png',
+    'name': 'ECommerce',
+    'content': `With Metaverse Estates, you have the power to design, customize, and inhabit your dream home in the metaverse. Want a castle in the clouds or a beachfront villa surrounded by digital waves....`,
+  },
+  {
+    'image': '/assets/nfts/MetaverseCards/Entertainments.png',
+    'name': 'Entertainments',
+    'content': `With Metaverse Estates, you have the power to design, customize, and inhabit your dream home in the metaverse. Want a castle in the clouds or a beachfront villa surrounded by digital waves....`,
+  },
+  {
+    'image': '/assets/nfts/MetaverseCards/Automobile.png',
+    'name': 'Automobile',
+    'content': `With Metaverse Estates, you have the power to design, customize, and inhabit your dream home in the metaverse. Want a castle in the clouds or a beachfront villa surrounded by digital waves....`,
+  },
+  {
+    'image': '/assets/nfts/MetaverseCards/Sport & Nutrition.png',
+    'name': 'Sport & Nutrition',
+    'content': `With Metaverse Estates, you have the power to design, customize, and inhabit your dream home in the metaverse. Want a castle in the clouds or a beachfront villa surrounded by digital waves....`,
+  },
+  {
+    'image': '/assets/nfts/MetaverseCards/Education.png',
+    'name': 'Education',
+    'content': `With Metaverse Estates, you have the power to design, customize, and inhabit your dream home in the metaverse. Want a castle in the clouds or a beachfront villa surrounded by digital waves....`,
+  },
+  {
+    'image': '/assets/nfts/MetaverseCards/Gaming.png',
+    'name': 'Gaming',
+    'content': `With Metaverse Estates, you have the power to design, customize, and inhabit your dream home in the metaverse. Want a castle in the clouds or a beachfront villa surrounded by digital waves....`,
+  },
+  {
+    'image': '/assets/nfts/MetaverseCards/Dispensary.png',
+    'name': 'Dispensary',
+    'content': `With Metaverse Estates, you have the power to design, customize, and inhabit your dream home in the metaverse. Want a castle in the clouds or a beachfront villa surrounded by digital waves....`,
+  },
+  {
+    'image': '/assets/nfts/MetaverseCards/Cryptocurrency.png',
+    'name': 'Cryptocurrency',
+    'content': `With Metaverse Estates, you have the power to design, customize, and inhabit your dream home in the metaverse. Want a castle in the clouds or a beachfront villa surrounded by digital waves....`,
+  },
+  {
+    'image': '/assets/nfts/MetaverseCards/Hotel.png',
+    'name': 'Hotel',
+    'content': `With Metaverse Estates, you have the power to design, customize, and inhabit your dream home in the metaverse. Want a castle in the clouds or a beachfront villa surrounded by digital waves....`,
+  },
+  {
+    'image': '/assets/nfts/MetaverseCards/Restaurant.png',
+    'name': 'Restaurant',
+    'content': `With Metaverse Estates, you have the power to design, customize, and inhabit your dream home in the metaverse. Want a castle in the clouds or a beachfront villa surrounded by digital waves....`,
+  },
+  {
+    'image': '/assets/nfts/MetaverseCards/Marketplace.png',
+    'name': 'Marketplace',
+    'content': `With Metaverse Estates, you have the power to design, customize, and inhabit your dream home in the metaverse. Want a castle in the clouds or a beachfront villa surrounded by digital waves....`,
+  }
+]
 
 BuildMetaverse.getLayout = (page: React.ReactElement) => <DashboardLayout>{page}</DashboardLayout>;
+
+const Bar = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+
+  // Media query for responsive mode
+  '@media (min-width: 768px)': {
+    flexDirection: 'row',
+  },
+})
+
+const Comp = styled('div') ({
+  display: 'flex',
+  gap: '20px',
+  flexDirection: 'column',
+
+  // Media query for responsive mode
+  '@media (min-width: 768px)': {
+    flexDirection: 'row',
+  },
+})
 
 // ----------------------------------------------------------------------
 export default function BuildMetaverse() {
@@ -24,9 +107,48 @@ export default function BuildMetaverse() {
       <Head>
         <title> Build Metaverse </title>
       </Head>
-      <Stack sx={{width:'100%' , height:'100vh' , boxShadow: '0px 6px 12px 0px #00000033',
-        background: '#202324'}}>
-            Build Metaverse
+      <Stack sx={{width:'100%' , height:'calc(100vh - 120px)' , boxShadow: '0px 6px 12px 0px #00000033',
+        background: '#202324', color: 'white', fontFamily: "Neue Haas Grotesk Display Pro", padding: '32px', gap:'20px'}}>
+            <Bar>
+              <div style={{fontSize: '20px', fontWeight: '600'}}>Please select a metaverse area to build in</div>
+              <Comp>
+                <TextField
+                    variant='standard'
+                    InputProps={{
+                        disableUnderline: true,
+                        endAdornment: (
+                            <InputAdornment position='end'>
+                                <Icon icon="octicon:search-16" />
+                            </InputAdornment>
+                        ),
+                        style: { color: 'white' }
+                    }}
+                    placeholder='Search Metaverse'
+                    style={{ color: 'white', borderRadius: '15px', border: 'solid 1px gray', padding: '5px 10px 5px 10px', width: '100%' }}
+                />
+                <TextField
+                    variant='standard'
+                    InputProps={{
+                        disableUnderline: true,
+                        endAdornment: (
+                            <InputAdornment position='end'>
+                                <Icon icon="formkit:down" />
+                            </InputAdornment>
+                        ),
+                        style: { color: 'white' }
+                    }}
+                    placeholder='Newly Listed'
+                    style={{ color: 'white', borderRadius: '15px', border: 'solid 1px gray', padding: '5px 10px 5px 10px' }}
+                />
+              </Comp>
+            </Bar>
+            <div style={{display: 'flex', alignItems: 'flex-start', alignContent: 'flex-start', gap: '20px', flexWrap: 'wrap', height: 'calc(100% - 100px)', overflowY:'scroll', padding:'20px'}}>
+                    {
+                      MetaverseItem.map(item => (
+                        <MetaverseCard image={item.image} name={item.name} content={item.content} />
+                      ))
+                    }
+            </div>
       </Stack>
     </>)
 }
