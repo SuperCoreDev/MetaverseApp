23b6bf7a12817d201e92a3e7c4666030d963405f
SuperCoreDev <coredevsantos@gmail.com>
2023-11-09 02:53:46 -0500

    realestatepagecomplete

--- src/pages/metaverse/buildmetaverse/realestate/index.tsx
+++ src/pages/metaverse/buildmetaverse/realestate/index.tsx
@@ -6,38 +6,58 @@ import Image from 'next/image';
 import NextLink from 'next/link'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { useRef, ReactNode, useState, useEffect , useCallback } from 'react';
-import { styled } from '@mui/material/styles';
+import { styled, ThemeProvider } from '@mui/material/styles';
 import SearchIcon from "@mui/icons-material/Search";
 import NextBreadcrumbs from 'src/components/NextBreadCrumbs';
 import { Icon } from '@iconify/react';
-import { Container, Typography, Link, Stack, Button, Box, Select, MenuItem, Grid, TextField, InputAdornment } from '@mui/material';
+import { Container, Tabs,Tab,Typography, Link, Stack, Button, Box, Select, MenuItem, Grid, TextField, InputAdornment,createTheme } from '@mui/material';
 
 // layouts
 import DashboardLayout from '../../../../layouts/dashboard';
 import Scrollbar from 'src/components/scrollbar/Scrollbar';
 import useResponsive from 'src/hooks/useResponsive';
+import { useTheme } from '@emotion/react';
 
 
 
 
 // components
+type StyledButtonProps = {
+    bg : 'string',
+    color : 'string'
+}
+const StyledButton = styled(Button , {
+    shouldForwardProp:(prop) => prop !== 'bg' && prop !== 'color'})<StyledButtonProps>(({bg , color}) => ({
+   
+            background: bg,
+            color,
+            borderRadius: '50px',
+            padding:'8px 12px',
+            textAlign: 'center',
+            border: 'solid 1px gray',
+            fontSize:'18px',
+            '@media (max-width:700px)' :{
+                fontSize:'13px'
+            },
+            '&:hover':{
+                background:'#23b3e5',
+                border:'none'
+            },
+   
+}))
 
-const MetaverseClassifyButton = styled('div')({
-    display: 'flex',
-    width: '138px',
-    height: '41px',
-    color: '#BEBCBE',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: '4px',
-    flex: '1 0 0',
-    borderRadius: '8px',
-    border: '1px solid #2B2928',
-    background: '#191919',
-    fontSize: '14',
-    fontWeight: '500'
+const IconsButton = styled('div')({
+    width:'24px',
+    height:'24px',
+    
+    transition: 'width 0.3s ease-in-out,height 0.5s cubic-bezier(0.215, 0.61, 0.355, 1),transform 1s linear',
+
+    '&:hover':{
+        width:'48px',
+        height:'48px',
+        transform : 'translate(-24,-24)',
+    }
 })
-
 const Wrapper = styled(Stack)({
     flexDirection: 'column',
     borderRadius: '16px',
@@ -63,15 +83,15 @@ type CardWrapperProps = {
 export const CardWrapper = ({ src }: CardWrapperProps) => (
 
     <Stack direction='column' padding='14px' gap="17px" 
-        sx={{ borderRadius: '16px', border: '1px solid #4A4D52', background: '#202324' , minWidth:'422px'}}>
+        sx={{ borderRadius: '16px', border: '1px solid #4A4D52', background: '#202324'}}>
         <Box position='relative' width='inherit'>
             <Image src={src} alt="SFR" width={393} height={208} />
             <Box position='absolute' top={15} left={19}>
-                <Stack direction='row' padding='8px' gap='20px' borderRadius='4px' sx={{ background: 'rgba(12, 13, 14, 0.20)' }}>
-                    <Icon icon="tabler:capture" color="white" width="24" height="24" />
-                    <Icon icon="material-symbols-light:share-outline" color="white" width="24" height="24" />
-                    <Icon icon="material-symbols-light:bookmark-outline" color="white" width="24" height="24" />
-                    <Icon icon="uiw:like-o" color="white" width="24" height="24" />
+                <Stack direction='row'  height={48} justifyContent='center' alignItems='center' padding='8px' gap='20px' borderRadius='4px' sx={{ background: 'rgba(12, 13, 14, 0.20)' }}>
+                    <IconsButton><Icon icon="tabler:capture" color="white" width="100%" height="100%" /></IconsButton>
+                    <IconsButton><Icon icon="material-symbols-light:share-outline" color="white" width="100%" height="100%" /></IconsButton>
+                    <IconsButton><Icon icon="material-symbols-light:bookmark-outline" color="white" width="100%" height="100%" /></IconsButton>
+                    <IconsButton><Icon icon="uiw:like-o" color="white" width="100%" height="100%" /></IconsButton>
                 </Stack>
             </Box>
         </Box>
@@ -103,6 +123,9 @@ RealEstate.getLayout = (page: React.ReactElement) => <DashboardLayout>{page}</Da
 
 export default function RealEstate() {
     const isLargeDesktop = useResponsive('up',1100)
+    const isDesktop = useResponsive('down' , 970)
+    const isSmall = useResponsive('down' ,'md')
+    const ref = useRef<HTMLDivElement>(null);
     const getDefaultTextGenerator = useCallback((subpath: string) => (
         {
             "buildmetaverse": "Build Metaverse",
@@ -117,51 +140,80 @@ export default function RealEstate() {
     ), []);
     const mainRef = useRef<HTMLDivElement>(null);
     useEffect(()=>{
+        handlerButtonClick(0);
         if(mainRef.current)
         {
         mainRef.current.style.marginTop = '0'
         mainRef.current.style.transition = '1s ease'
         }
     },[])
+    const handlerButtonClick = (id:number)  => {
+        if(ref.current){
+           const styledButtonElements = Array.from(ref.current.childNodes).filter((node) : node is HTMLDivElement => node instanceof HTMLDivElement)
+           styledButtonElements.forEach((node)=>{
+            const children : HTMLElement = node?.firstChild;
+            children.style.color = 'white',
+            children.style.backgroundColor = 'transparent'
+           })
+           const indexElement : HTMLElement = styledButtonElements[id]?.firstChild;
+           
+           indexElement.style.color= 'black';
+           indexElement.style.backgroundColor='#D96BFF'
+        }
+    }
+    const theme = useTheme();
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+    function a11yProps(index: number) {
+        return {
+          id: `simple-tab-${index}`,
+          'aria-controls': `simple-tabpanel-${index}`,
+        };
+    
+    }
     return (
+        <ThemeProvider theme={theme}>
         <Stack ref={mainRef} marginTop='100%' fontFamily='Neue Haas Grotesk Display Pro' direction='column' sx={{}} gap='24px' padding='0 24px 24px 24px'>
             <NextBreadcrumbs
                 breadcrumbCase
                 getDefaultTextGenerator={getDefaultTextGenerator}
                 getTextGenerator={getTextGenerator} />
-            <Grid container  justifyContent='space-between'>
+            <Grid container ref={ref} justifyContent='space-between' gap={2}>
                 <Grid item>
-                    <MetaverseClassifyButton>Real Estate</MetaverseClassifyButton>
+                    <StyledButton color='white' onClick={()=>handlerButtonClick(0)}>Real Estate</StyledButton>
                 </Grid>
                 <Grid item>
-                    <MetaverseClassifyButton>Ecommerce</MetaverseClassifyButton>
+                    <StyledButton color='white' onClick={()=>handlerButtonClick(1)}>Ecommerce</StyledButton>
                 </Grid>
                 <Grid item>
-                    <MetaverseClassifyButton>Entertainment</MetaverseClassifyButton>
+                    <StyledButton color='white' onClick={()=>handlerButtonClick(2)}>Entertainment</StyledButton>
                 </Grid>
                 <Grid item>
-                    <MetaverseClassifyButton>AutoMobile</MetaverseClassifyButton>
+                    <StyledButton color='white' onClick={()=>handlerButtonClick(3)}>AutoMobile</StyledButton>
                 </Grid>
                 <Grid item>
-                    <MetaverseClassifyButton>Sport & Nutrition</MetaverseClassifyButton>
+                    <StyledButton color='white' onClick={()=>handlerButtonClick(4)}>Sport & Nutrition</StyledButton>
                 </Grid>
                 <Grid item>
-                    <MetaverseClassifyButton>Education</MetaverseClassifyButton>
+                    <StyledButton color='white' onClick={()=>handlerButtonClick(5)}>Education</StyledButton>
                 </Grid>
                 <Grid item>
-                    <MetaverseClassifyButton>Gaming</MetaverseClassifyButton>
+                    <StyledButton color='white' onClick={()=>handlerButtonClick(6)}>Gaming</StyledButton>
                 </Grid>
                 <Grid item>
-                    <MetaverseClassifyButton>
+                    <StyledButton color='white' onClick={()=>handlerButtonClick(7)}>
                         More
                         <Icon icon="formkit:down" color="gray" width="24" height="24" />
-                    </MetaverseClassifyButton>
+                    </StyledButton>
                 </Grid>
             </Grid>
             <Wrapper>
-                <Stack direction='row' justifyContent='space-between'>
+                <Stack sx={{flexDirection: isDesktop ?'column' : 'row'}} gap="20px" justifyContent='space-between'>
                     <Typography color='white' fontSize={32} fontWeight={700}>Real Estate</Typography>
-                    <Stack direction='row' gap='24px'>
+                    <Stack sx={{flexDirection : isSmall ? 'column' : 'row'}} gap='24px'>
                         <TextField
                             id="search"
                             type="search"
@@ -169,7 +221,8 @@ export default function RealEstate() {
                             // value={searchTerm}
                             // onChange={handleChange}
                             placeholder='Search...'
-                            sx={{ width: 228, borderRadius: '12px', background: '#1E2121', }}
+                            // eslint-disable-next-line no-nested-ternary
+                            sx={{ width: isSmall ? '100%' : isDesktop ? '50%' : 228 , borderRadius: '12px', background: '#1E2121', }}
                             InputProps={{
                                 sx: { color: 'white' },
                                 endAdornment: (
@@ -181,41 +234,54 @@ export default function RealEstate() {
                         />
                         <Select
                             sx={{
-                                color: 'white', width: '208px', borderRadius: '12px', background: '#1E2121'
+                                color: 'white', width :  isSmall ? '100%' : isDesktop ? '50%' : 208, borderRadius: '12px', background: '#1E2121'
                             }}
                         >
-                            <MenuItem value={1}>Red</MenuItem>
+                            {/* <MenuItem value={1}>Red</MenuItem> */}
                         </Select>
                     </Stack>
                 </Stack>
-                <Stack direction='row' gap="32px">
-                    <Box>
-                        <Stack width={268} color='white' borderRadius='16px' sx={{ background: '#202324' }} direction='column' padding='4px 8px'>
-                            <Box padding='12px' textAlign='center' sx={{ color: 'red', border: '1px solid transparent', borderBottomColor: '#3A3D41' }}>All</Box>
-                            <Box padding='12px' textAlign='center' sx={{ border: '1px solid transparent', borderBottomColor: '#3A3D41' }}>Residental</Box>
-                            <Box padding='12px' textAlign='center' sx={{ border: '1px solid transparent', borderBottomColor: '#3A3D41' }}>Commercial</Box>
-                            <Box padding='12px' textAlign='center' sx={{ border: '1px solid transparent', borderBottomColor: '#3A3D41' }}>NewCondo</Box>
-                            <Box padding='12px' textAlign='center' sx={{ border: '1px solid transparent' }}>Land</Box>
+                <Stack direction={isDesktop ? 'column' : 'row'} gap="32px">
+                    {isDesktop ? (<Box sx={{ width: '100%' }}>
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                            <Tabs textColor="primary" indicatorColor="secondary" 
+                            variant="scrollable" scrollButtons allowScrollButtonsMobile
+                            value={value} onChange={handleChange} aria-label="tab"
+                            sx={{'& .MuiTabs-scrollButtons' : {color:'white'}}}>
+                                <Tab label="All" {...a11yProps(0)} />
+                                <Tab label="Residental" {...a11yProps(1)} />
+                                <Tab label="Commercial" {...a11yProps(2)} />
+                                <Tab label="NewCondo" {...a11yProps(3)} />
+                                <Tab label="Land" {...a11yProps(4)} />
+                            </Tabs>
+                        </Box>
+                    </Box>):(<Box>
+                        <Stack width={268} color='white' borderRadius='16px' sx={{cursor:'pointer', background: '#202324' }} direction={isDesktop?'row':'column'} padding='4px 8px'>
+                            <Box padding='12px' textAlign='center' sx={{ borderBottom : '1px solid #3A3D41', color: 'red' }}>All</Box>
+                            <Box padding='12px' textAlign='center' sx={{ borderBottom : '1px solid #3A3D41' }}>Residental</Box>
+                            <Box padding='12px' textAlign='center' sx={{ borderBottom : '1px solid #3A3D41' }}>Commercial</Box>
+                            <Box padding='12px' textAlign='center' sx={{ borderBottom : '1px solid #3A3D41' }}>NewCondo</Box>
+                            <Box padding='12px' textAlign='center' sx={{ borderBottom : 'none' }}>Land</Box>
                         </Stack>
-                    </Box>
+                    </Box>)}
                     <Stack direction='column' sx={{ width: '100%' }} gap="26px" >
-                        <Stack direction='row' borderRadius='16px' >
-                            <Box position='relative' width='inherit' >
+                        <Stack direction={isLargeDesktop ? 'row' : 'column'} borderRadius='16px' width='100%'>
+                            <Box position='relative' width={isDesktop ? '100%' : 'inherit'} >
                                 <img src='/assets/images/metaverse/realestate/SFR1.png' alt="SFR" style={{ width: '100%', height: '100%' }} />
                                 <Box position='absolute' top={15} left={19}>
-                                    <Stack direction='row' padding='8px' gap='20px' borderRadius='4px' sx={{ background: 'rgba(12, 13, 14, 0.20)' }}>
-                                        <Icon icon="tabler:capture" color="white" width="24" height="24" />
-                                        <Icon icon="material-symbols-light:share-outline" color="white" width="24" height="24" />
-                                        <Icon icon="material-symbols-light:bookmark-outline" color="white" width="24" height="24" />
-                                        <Icon icon="uiw:like-o" color="white" width="24" height="24" />
+                                    <Stack direction='row' justifyContent='center' alignItems='center' padding='8px' height={48} gap='20px' borderRadius='4px' sx={{ background: 'rgba(12, 13, 14, 0.20)' }}>
+                                        <IconsButton><Icon icon="tabler:capture" color="white" width='100%' height='100%' /></IconsButton>
+                                        <IconsButton><Icon icon="material-symbols-light:share-outline" color="white" width='100%' height='100%' /></IconsButton>
+                                        <IconsButton><Icon icon="material-symbols-light:bookmark-outline" color="white" width='100%' height='100%' /></IconsButton>
+                                        <IconsButton><Icon icon="uiw:like-o" color="white" width='100%' height='100%' /></IconsButton>
                                     </Stack>
                                 </Box>
                             </Box>
-                            <Stack direction='column' sx={{ width: '100%' }}>
+                            <Stack direction='column' sx={{ width: '100%'}}>
                                 <Stack direction='column' gap="2px"
                                     sx={{
                                         padding: '24px 20px 10px 20px',
-                                        borderTopRightRadius: '16px', background: '#C40941'
+                                        borderTopRightRadius: isLargeDesktop ? '16px' : 'none', background: '#C40941',
                                     }}>
                                     <Typography color='white' fontSize={20} fontWeight={700}>Single Family Residential</Typography>
                                     <Box sx={{ width: '100%', height: '1px', background: '#D03A67' }} />
@@ -232,7 +298,8 @@ export default function RealEstate() {
                                     <Typography color='#edb3c4'>price/sqft</Typography>
                                 </Stack>
                                 <Stack justifyContent='center' alignItems='center'
-                                    sx={{ flexGrow: '1', background: 'black', borderBottomRightRadius: '16px', padding: '16px 0' }}>
+                                    sx={{ flexGrow: '1', background: 'black', borderBottomRightRadius: '16px', padding: '16px 0',
+                                    borderBottomLeftRadius: isLargeDesktop ? 'none' : '16px' , WebkitBorderBottomRightRadius: isLargeDesktop? 'none' : '16px' }}>
                                     <Typography color='white'>Take A Tour</Typography>
                                 </Stack>
                             </Stack>
@@ -257,5 +324,6 @@ export default function RealEstate() {
                 </Stack>
             </Wrapper>
         </Stack>
+        </ThemeProvider>
     )
 }
